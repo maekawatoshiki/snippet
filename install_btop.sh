1e22b640bc5a52b609219d27799bf191f8f0f103
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-12-15 10:42:16 +0900

    install_btop.sh: Add guard for x86_64 Linux

--- install_btop.sh
+++ install_btop.sh
@@ -2,6 +2,11 @@
 
 VERSION=1.2.13
 
+if [ "$(uname -m)" != "x86_64" -or "$(uname -s)" != "Linux" ]; then
+  echo "This script only works on x86_64 Linux."
+  exit 1
+fi
+
 wget https://github.com/aristocratos/btop/releases/download/v$VERSION/btop-x86_64-linux-musl.tbz
 tar xvf btop-x86_64-linux-musl.tbz
 (cd btop && ./install.sh)
